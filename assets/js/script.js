ceef0f773c746d59ae2179c52678505ea2578632
schipanic2 <schipanic2@winthrop.edu>
2017-04-25 01:07:26 +0000

    fixed the nested each problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,30 @@
 /* global $ */
 $(document).ready(function(){
     
-    // showBackground("mountain");
+    showBackground("mountain");
     // showBackground("canyon");
-    showBackground("volcano");
-
-
+    // showBackground("volcano");
 
 });
 
-
-
 function showBackground(divID){
     divID = "#"+divID;
-    var background = "";
-    var imgs = [];
-        $(divID).each(function(){
-            $(this).children('img').each(function(i){
-                if(i === 0){
-                    background = $(this).attr('id');
-                }else{
-                    imgs.push($(this).attr('id'));
-                }
-            });
-        });
+    var backgroundID = "";
+    var imgArray = [];
+    
+    $(divID+">img").each(function(i){
+        if(i === 0){
+            backgroundID = $(this).attr('id');
+        }else{
+            imgArray.push($(this).attr('id'));
+        }
+    });
 
-    $("#"+background).fadeTo('slow',1 , function(){
-        for (var i = 0; i < imgs.length; i++) {
+    $("#"+backgroundID).fadeTo('slow',1 , function(){
+        for (var i = 0; i < imgArray.length; i++) {
             (function(index) {
                 setTimeout(function() { 
-                    $("#"+imgs[index]).addClass('animated fadeInUp');
+                    $("#"+imgArray[index]).addClass('animated fadeInUp');
                 }, i * 550);
             })(i);
         }
