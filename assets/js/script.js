e5e1dcb05419601f44975b65fd278a368ee42c63
schipanic2 <schipanic2@winthrop.edu>
2017-04-25 03:44:32 +0000

    questions css, working on animations to get in sync

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 /* global $ */
 $(document).ready(function(){
-    
-    showBackground("mountain");
-    // showBackground("canyon");
-    // showBackground("volcano");
+    showBackground("mountain", 550);
+    showQuestionView("mountain", 600);
+    // showBackground("canyon", 400);
+    // showBackground("volcano", 500);
 
 });
 
-function showBackground(divID){
+function showBackground(divID, timeoutInterval){
     divID = "#"+divID;
     var backgroundID = "";
     var imgArray = [];
@@ -20,14 +20,29 @@ function showBackground(divID){
         }
     });
 
-    $("#"+backgroundID).fadeTo('slow',1 , function(){
+    $("#"+backgroundID).fadeTo('slow', 1 , function(){
         for (var i = 0; i < imgArray.length; i++) {
             (function(index) {
                 setTimeout(function() { 
                     $("#"+imgArray[index]).addClass('animated fadeInUp');
-                }, i * 550);
+                }, i * timeoutInterval);
             })(i);
         }
-        
+    });
+}
+
+function showQuestionView(divID, timeoutInterval){
+    divID = "#"+divID;
+    var $questionHeader = $(divID+">.question-view").find('h1');
+    var $questionBox = $(divID+">.question-view").find('.question-box');
+    var $questionOptions = $(divID+">.question-view").find('.question-option');
+    $questionHeader.addClass('animated fadeInUp');
+    $questionBox.addClass('animated fadeInUp');
+    
+    $.each($questionOptions, function (i) {
+        setTimeout(function() {
+            $(this).addClass('animated fadeInLeft');
+        }.bind(this), i * timeoutInterval);
     });    
+    
 }
