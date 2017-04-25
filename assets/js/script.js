e56fc9d651380770b696d559ff2adbea8a5b4120
schipanic2 <schipanic2@winthrop.edu>
2017-04-25 00:36:03 +0000

    preliminary animations for each page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,55 @@
 /* global $ */
 $(document).ready(function(){
+    var mountains = ["#ridge1",
+                     "#ridge2",
+                     "#ridge3",
+                     "#ridge4",
+                     "#sun"]; 
 
+    var canyons = ["#coll1",
+                  "#coll2",
+                  "#coll3",
+                  "#coll4",
+                  "#coll5",
+                  "#colr1",
+                  "#colr2",
+                  "#colr3",
+                  "#colr4",
+                  "#road"];
     
+    var volcanos = ["#step1",
+                    "#step2",
+                    "#step3",
+                    "#step4",
+                    "#step5",
+                    "#surf",
+                    "#mtn1",
+                    "#mtn2",
+                    "#volcanoImg",
+                    "#lava"
+                   ];
+
+
+    showBackground(mountains, "#mtn-background");
+    // showBackground(canyons, "#cyn-background");
+    // showBackground(volcanos, "#volcano-background");
+
+
+
 });
+
+
+
+function showBackground(array, backgroundID){
+    console.log(array, backgroundID);
+    $(backgroundID).fadeTo('slow',1 , function(){
+        for (var i = 0; i < array.length; i++) {
+            (function(index) {
+                setTimeout(function() { 
+                    console.log(array[index]); 
+                    $(array[index]).addClass('animated fadeInUp');
+                }, i * 550);
+            })(i);
+        }
+    });    
+}
