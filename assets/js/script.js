ec3aaf6f4eb14e2f1ae3de1ddcb0bf62f747a958
schipanic2 <schipanic2@winthrop.edu>
2017-04-25 00:53:06 +0000

    the run time on this isn't idea because of the nestnested for eaches but this doesn't rely on the arrays, just have to put it in the order of how we want it to show on the screen in the HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,9 @@
 /* global $ */
 $(document).ready(function(){
-    var mountains = ["#ridge1",
-                     "#ridge2",
-                     "#ridge3",
-                     "#ridge4",
-                     "#sun"]; 
-
-    var canyons = ["#coll1",
-                  "#coll2",
-                  "#coll3",
-                  "#coll4",
-                  "#coll5",
-                  "#colr1",
-                  "#colr2",
-                  "#colr3",
-                  "#colr4",
-                  "#road"];
     
-    var volcanos = ["#step1",
-                    "#step2",
-                    "#step3",
-                    "#step4",
-                    "#step5",
-                    "#surf",
-                    "#mtn1",
-                    "#mtn2",
-                    "#volcanoImg",
-                    "#lava"
-                   ];
-
-
-    showBackground(mountains, "#mtn-background");
-    // showBackground(canyons, "#cyn-background");
-    // showBackground(volcanos, "#volcano-background");
+    // showBackground("mountain");
+    // showBackground("canyon");
+    showBackground("volcano");
 
 
 
@@ -40,16 +11,28 @@ $(document).ready(function(){
 
 
 
-function showBackground(array, backgroundID){
-    console.log(array, backgroundID);
-    $(backgroundID).fadeTo('slow',1 , function(){
-        for (var i = 0; i < array.length; i++) {
+function showBackground(divID){
+    divID = "#"+divID;
+    var background = "";
+    var imgs = [];
+        $(divID).each(function(){
+            $(this).children('img').each(function(i){
+                if(i === 0){
+                    background = $(this).attr('id');
+                }else{
+                    imgs.push($(this).attr('id'));
+                }
+            });
+        });
+
+    $("#"+background).fadeTo('slow',1 , function(){
+        for (var i = 0; i < imgs.length; i++) {
             (function(index) {
                 setTimeout(function() { 
-                    console.log(array[index]); 
-                    $(array[index]).addClass('animated fadeInUp');
+                    $("#"+imgs[index]).addClass('animated fadeInUp');
                 }, i * 550);
             })(i);
         }
+        
     });    
 }
